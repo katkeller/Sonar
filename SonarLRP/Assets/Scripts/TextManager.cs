eb0000aa0a0104586692f024d830c1d9cc57a977
Kat Keller <kat.kek.keller@gmail.com>
2019-07-09 18:54:40 -0500

    Creaetd text manager script that should play an audio file for each line of dialogue, wait for the clip to finish, and then move on to the next line

---
+++ SonarLRP/Assets/Scripts/TextManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+
+public class TextManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset inkAsset;
+
+    [SerializeField]
+    private AudioClip[] dialogueClip = new AudioClip[5];
+
+    private Story inkStory;
+    private AudioSource audioSource;
+    //private float[] dialogueClipLength = new float[5];
+    private string currentAudioTag = "";
+    //private List<string> currentTagsList = new List<string>();
+    private AudioClip clipToPlay;
+    //private float clipToPlayLength = 0;
+    private int clipToPlayNumber;
+
+    private void Awake()
+    {
+        inkStory = new Story(inkAsset.text);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    IEnumerator WaitForAudioDialogue()
+    {
+        while (inkStory.canContinue)
+        {
+            Debug.Log(inkStory.Continue());
+            //currentTagsList = inkStory.currentTags;
+            currentAudioTag = string.Join("", inkStory.currentTags.ToArray());
+            //clipToPlayNumber = Int32.Parse(currentTagsList);
+            clipToPlayNumber = Int32.Parse(currentAudioTag);
+            clipToPlay = dialogueClip[clipToPlayNumber];
+            //clipToPlayLength = clipToPlay.length;
+            yield return new WaitForSeconds(clipToPlay.length);
+        }
+
+        if (inkStory.currentChoices.Count > 0)
+        {
+            for (int i = 0; i < inkStory.currentChoices.Count; i++)
+            {
+                Choice choice = inkStory.currentChoices[i];
+                Debug.Log("Choice " + (i + 1) + ". " + choice.text);
+            }
+        }
+    }
+}
--- TestConvo.ink
+++ TestConvo.ink
@@ -1,16 +1,16 @@
 -> start
 === start ===
-Friend: So I guess we have some time to catch up now.
-You: I guess. Or we could focus on what we're--
-Friend: So what have you been up to since high school? I heard you went to the University of Washington. That's pretty far from home, especially by Roosevelt standards. Were you running from something?
+Friend: So I guess we have some time to catch up now. #1
+You: I guess. Or we could focus on what we're-- #2
+Friend: So what have you been up to since high school? I heard you went to the University of Washington. That's pretty far from home, especially by Roosevelt standards. Were you running from something? #3
 *   [I had to leave.] -> start.had_to_leave
 *   [No, I just like Seattle.] -> start.like_seattle
     ->END
 = had_to_leave
-You: Yep, I had to get as far from home as possible.
+You: Yep, I had to get as far from home as possible. #4
 -> END
 = like_seattle
-You: No, I wasn't running away. I just liked it there more than here... or anywhere else, I guess. Plus, they gave me a pretty good scholarship.
+You: No, I wasn't running away. I just liked it there more than here... or anywhere else, I guess. Plus, they gave me a pretty good scholarship. #5
 Friend: Ha, yeah. And we all know how much you needed that.
 You: What?
 Friend: I-- Shit, I'm really sorry. We all made a lot of jokes like that way back when, but that came off way bitcher than I remember.
