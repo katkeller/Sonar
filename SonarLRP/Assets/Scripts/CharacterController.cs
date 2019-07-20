85436dd767402abb33cc82aa6d3434546eba4798
Kat Keller <kat.kek.keller@gmail.com>
2019-07-20 17:49:15 -0500

    Created simple player movement script and attached to to the player

---
+++ SonarLRP/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10.0f;
+
+    void Update()
+    {
+        Vector2 translation = SteamVR_Actions._default.Move.GetAxis(SteamVR_Input_Sources.Any) * speed;
+        translation *= Time.deltaTime;
+        transform.Translate(translation.x, 0, translation.y);
+    }
+}
